feef18cac0c9d568bf3202a59af6ecbcb1c0d4bd
KATO Hayate <dev@hayatek.jp>
2025-08-31 22:14:16 +0900

    chore: 設定ファイルを更新

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+export default {
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+
+  images: {
+    unoptimized: false,
+  },
+
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+} satisfies NextConfig;
