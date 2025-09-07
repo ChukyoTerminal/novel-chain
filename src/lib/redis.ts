c0c5b7a5c811cea688f6daeb28e44edbe26a6996
KATO Hayate <dev@hayatek.jp>
2025-09-07 13:20:54 +0900

    Revert "fix: ビルドエラーを修正"
    This reverts commit 7225675fa844db7d5a166f3876e2319a1fddc842.

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,3 +1,5 @@
+/* eslint-disable sonarjs/fixme-tag */
+
 import { createClient } from 'redis';
 
 import type { RedisClientType } from 'redis';
@@ -5,11 +7,17 @@ import type { RedisClientType } from 'redis';
 
 const globalForRedis = globalThis as unknown as { redis: RedisClientType };
 
+// FIXME: ビルド時にもREDIS_URLが必要
 export const redis =
   globalForRedis.redis ||
   await createClient({ url: process.env.REDIS_URL })
     .on('error', async (e) => {
       console.error('Redis Client Error:', e);
+      if (!redis.isOpen) {
+        console.info('Reconnecting to Redis...');
+        await redis.connect();
+        console.info('Reconnected to Redis');
+      }
     })
     .connect();
 
